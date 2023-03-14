5b79018daca878ed4196bcb63f68c8c06d67adcc
tntanja <tanjaa_88@hotmail.com>
2023-03-14 18:41:30 +0200

    react projekti 7.3.

---
+++ tanjati-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+
+import { Message } from "./pages/Message";
+import { Buttons } from "./pages/Buttons";
+
+
+function App() {
+  return (
+    <div>
+      <Message />
+      <Buttons />
+    </div>
+  );
+}
+
+export default App;
